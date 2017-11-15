dde2cd588b5ef269b865d7c3cb765d95abfadca0
Mike S <money88@gmail.com>
2017-11-14 21:08:40 -0600

    Can use something similar for leaderboard

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppRegistry, View } from 'react-native';
 
 import Header from './src/components/header';
-import AlbumList from './src/components/AlbumList'
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
     <View>
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { View, Text } from 'react-native'
+import React, { Component } from 'react';
+import { View } from 'react-native'
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List!!!</Text>
-    </View>
-  );
-};
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => response.json())
+      .then(data => this.setState({ albums: data }));
+    // debugger;
+    // control + M will open menu to open Remote Debugger for android
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
